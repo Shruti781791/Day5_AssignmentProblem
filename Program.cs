c112e2c744b5bdc62ed9498225b48067b3eaa03a
Shruti <Kulkarnishruti@gmail.com>
2022-12-02 10:21:46 +0530

    Quotient

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Day5_assignment;
+using System;
+
+namespace Day5assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // FlipCoin.CheckCoin();
+            //leapyear.CheckYear();
+            Quotient.xyz();
+
+
+
+
+
+
+        }
+}
+}
---
+++ Quotient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_assignment
+{
+    public class Quotient
+
+    {
+        public static void xyz ()
+        {
+        int dividend = 50, divisor = 8;
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        Console.WriteLine("Dividend:{0} Divisor:{1}",dividend,divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
+        }
+}
+}
