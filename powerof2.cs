0dbf54cef30288b9fd3ba3ee7cc8e7a6dd5d38c3
Shruti <Kulkarnishruti@gmail.com>
2022-12-02 12:18:16 +0530

    powerof2

---
+++ powerof2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_assignment
+{
+    internal class powerof2
+    {
+        public static void power()
+            {
+            int e;
+            int result;
+
+            for (int i = 0; i < 10; i++)
+            {
+                result = 1;
+                e = i;
+
+                while (e > 0)
+                {
+                    result *= 2;
+                    e--;
+                }
+
+                Console.WriteLine("2 to the " + i + " power is " + result);
+            }
+        }
+    }
+}
+
+    
